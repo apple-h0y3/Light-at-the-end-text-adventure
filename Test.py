50b224611c5148a506e731141d3056d8908edc5d
apple-h0y3 <39070703+apple-h0y3@users.noreply.github.com>
2025-09-30 20:51:12 +0000

    sept 30 update f

--- Test.py
+++ Test.py
@@ -1,15 +1,18 @@
 import time
 bed=time.sleep
+#https://talyian.github.io/ansicolors/
+# meet the characters
+print("\x1b[38;5;200m this is annabelles voice!\033[0m")
+print("\x1b[38;5;219m these are annabelles thoughts!\033[0m")
 
-# Print text in red
-print('\033[31mWell hello there!\033[0m')
+print(" \x1b[38;5;220mMEET the Ringmaster!\033[0m")
+print("\x1b[38;5;97m this is the Ringmaster behind the scenes\033[0m")
 
-# Print text with a blue background
-print('\033[44mHello World but on Blue!\033[0m')
+print("\x1b[31m This is Silas! he's a vampire\033[0m")
+print("\x1b[38;5;124m this is what happens when silas is doing bad things! \033[m")
 
-# Print text in red with a blue background
-print('\033[31m\033[44mHello World but Red on Blue!\033[0m')
+print("\x1b[38;5;27m this is casper!\033[m")
+print("\x1b[38;5;39m this is casper when he's scared!\033[m")
 
-print("\033[38;5;206mThis text is pink.\033[0m")
-bed(3)
-print("\x1b[38;5;200mThis text is pink. \033[0m")
+print("meet the characters!")
+ 
